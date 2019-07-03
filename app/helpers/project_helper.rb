98def2653188aca42d1fe7f00765237941afa379
Eugene Haisenka <evheny0@gmail.com>
2019-07-03 11:53:29 +0300

    Show logo of integration on projects page

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -11,10 +11,16 @@ module ProjectHelper
   end
 
   def link_to_project_orgatnization(project)
-    return if project.integration_type == ProjectsConstants::Providers::VIA_SSH
+    return project_integration_logo(project) if project.integration_type == ProjectsConstants::Providers::VIA_SSH
 
     link_to(github_router.page_url(project.name), target: :_blank) do
-      image_pack_tag("media/images/logos/github-logo.png")
+      project_integration_logo(project)
     end + " "
+  end
+
+  def project_integration_logo(project)
+    return image_pack_tag("media/images/logos/git-logo-black.png", size: "32x32") if project.integration_type == ProjectsConstants::Providers::VIA_SSH
+
+    image_pack_tag("media/images/logos/github-logo.png", size: "32x32") if project.integration_type == ProjectsConstants::Providers::GITHUB
   end
 end
