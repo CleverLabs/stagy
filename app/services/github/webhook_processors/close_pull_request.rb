f46de83f53e0a9da3b6045b5acb011c31d4533f9
Nick Vyrko <nikita.vyrko@gmail.com>
2019-07-08 16:24:33 +0300

    [#78] Handle close pull request webhook

--- app/services/github/webhook_processors/close_pull_request.rb
+++ app/services/github/webhook_processors/close_pull_request.rb
@@ -4,11 +4,23 @@ module Github
   module WebhookProcessors
     class ClosePullRequest
       def initialize(body)
-        @body = body
+        @wrapped_body = Github::Events::PullRequest.new(payload: body)
+        @project = Project.find_by(integration_type: ProjectsConstants::Providers::GITHUB, integration_id: @wrapped_body.installation_id)
       end
 
       def call
-        ReturnValue.new(status: :no_action)
+        project_instance = @project.project_instances.find_by(attached_pull_request_number: @wrapped_body.number)
+
+        Deployment::Processes::DestroyProjectInstance.new(project_instance, get_user(@wrapped_body.sender)).call
+        ReturnValue.ok
+      end
+
+      private
+
+      def get_user(sender)
+        ::User.find_or_create_by!(auth_provider: ProjectsConstants::Providers::GITHUB, auth_uid: sender.id) do |user|
+          user.full_name = sender.login
+        end
       end
     end
   end
