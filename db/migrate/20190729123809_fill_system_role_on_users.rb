59220a213b7317b782d4578756f9f8104cba5e4a
Eugene Haisenka <evheny0@gmail.com>
2019-07-29 17:51:51 +0300

    Fix system_role migration

--- db/migrate/20190729123809_fill_system_role_on_users.rb
+++ db/migrate/20190729123809_fill_system_role_on_users.rb
@@ -5,7 +5,7 @@ class FillSystemRoleOnUsers < ActiveRecord::Migration[5.2]
 
   def up
     User.find_each do |user|
-      user.update_columns(system_role: "user")
+      user.update_columns(system_role: 0)
     end
   end
 end
