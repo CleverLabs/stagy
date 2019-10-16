fe31099f50209ea7194d57d771333ee05841d6b2
Eugene Haisenka <evheny0@gmail.com>
2019-10-16 10:35:12 +0300

    Fix migration

--- db/migrate/20191011085824_add_container_to_repositories.rb
+++ db/migrate/20191011085824_add_container_to_repositories.rb
@@ -1,5 +1,5 @@
 class AddContainerToRepositories < ActiveRecord::Migration[5.2]
   def change
-    add_column :repositories, :container, :integer, null: false, default: 0
+    safety_assured { add_column :repositories, :container, :integer, null: false, default: 0 }
   end
 end
