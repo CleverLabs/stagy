ec968d72f8f3468a93ce54711834c3a9586cda05
Nick Vyrko <nikita.vyrko@gmail.com>
2019-10-24 16:43:56 +0300

    [#185] Gitlab project icon

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module ProjectHelper
+  PROVIDER_IMAGE_PATH = {
+    ::ProjectsConstants::Providers::VIA_SSH => "media/images/logos/git-logo-black.svg",
+    ::ProjectsConstants::Providers::GITHUB => "media/images/logos/github-logo.svg",
+    ::ProjectsConstants::Providers::GITLAB => "media/images/logos/gitlab-logo.svg"
+  }.freeze
+
   def link_to_project_orgatnization(project)
     return project_integration_logo(project) if project.integration_type == ProjectsConstants::Providers::VIA_SSH
 
@@ -10,8 +16,7 @@ module ProjectHelper
   end
 
   def project_integration_logo(project)
-    return image_pack_tag("media/images/logos/git-logo-black.png", size: "32x32") if project.integration_type == ProjectsConstants::Providers::VIA_SSH
-
-    image_pack_tag("media/images/logos/github-logo.png", size: "32x32") if project.integration_type == ProjectsConstants::Providers::GITHUB
+    image_path = PROVIDER_IMAGE_PATH.fetch(project.integration_type)
+    image_pack_tag(image_path, size: "32x32")
   end
 end
