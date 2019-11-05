cef690ec6cfc4eae2ed2fafd423b2d90a296fb19
Eugene Haisenka <evheny0@gmail.com>
2019-11-05 14:45:27 +0300

    Remove git version from rollbar for now - docker fails during the build

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rollbar.configure do |config|
+  def git_sha_for_rollbar
+    # we don't copy .git folder, so we cannot know which commit it is
+    # Git.open(".").object("HEAD").sha
+    ""
+  end
+
   config.access_token = ENV["ROLLBAR_POST_SERVER_ITEM_TOKEN"]
-  config.code_version = Git.open(".").object("HEAD").sha
+  config.code_version = git_sha_for_rollbar
 
   config.before_process = proc do |options|
     raise Rollbar::Ignore if options[:exception].is_a?(ActionController::RoutingError)
