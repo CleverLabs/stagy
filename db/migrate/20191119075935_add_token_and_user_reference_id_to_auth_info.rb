b850a61f6971304e3dc549fedeb7ff60df3d011c
Eugene Haisenka <evheny0@gmail.com>
2019-12-19 18:09:48 +0300

    Fix strong migrations

--- db/migrate/20191119075935_add_token_and_user_reference_id_to_auth_info.rb
+++ db/migrate/20191119075935_add_token_and_user_reference_id_to_auth_info.rb
@@ -1,8 +1,10 @@
 class AddTokenAndUserReferenceIdToAuthInfo < ActiveRecord::Migration[5.2]
   def change
-    add_column :auth_infos, :token, :string
-    add_column :auth_infos, :primary, :boolean, default: false
-    add_column :auth_infos, :email, :string
-    add_reference :auth_infos, :user_reference, foreign_key: true, null: true, index: false
+    safety_assured do
+      add_column :auth_infos, :token, :string
+      add_column :auth_infos, :primary, :boolean, default: false
+      add_column :auth_infos, :email, :string
+      add_reference :auth_infos, :user_reference, foreign_key: true, null: true, index: false
+    end
   end
 end
