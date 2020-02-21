4a627d8ba5ceb70813c747aadd3ec9934deb7a79
Eugene Haisenka <evheny0@gmail.com>
2020-02-21 15:16:49 +0300

    Fix s3 permission

--- app/services/deployment/configuration_builders/name_builder.rb
+++ app/services/deployment/configuration_builders/name_builder.rb
@@ -4,7 +4,12 @@ module Deployment
   module ConfigurationBuilders
     class NameBuilder
       def application_name(project_name, project_id, repo_name, instance_name)
-        Utils::NameSanitizer.sanitize_downcase("#{project_name}-#{project_id}-#{repo_name}-#{instance_name}")
+        name = application_name_prefix(project_name, project_id) + "#{repo_name}-#{instance_name}"
+        Utils::NameSanitizer.sanitize_downcase(name)
+      end
+
+      def application_name_prefix(project_name, project_id)
+        Utils::NameSanitizer.sanitize_downcase("#{project_name}-#{project_id}-")
       end
 
       def external_project_name(project_name, project_id)
