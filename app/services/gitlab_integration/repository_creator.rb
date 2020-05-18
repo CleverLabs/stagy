f815dbd67a747d73639c4e6e430b50ca50c40e88
Eugene Haisenka <evheny0@gmail.com>
2020-05-18 16:02:49 +0300

    Perform 'on repo creation' callback for gitlab too

--- app/services/gitlab_integration/repository_creator.rb
+++ app/services/gitlab_integration/repository_creator.rb
@@ -13,8 +13,16 @@ module GitlabIntegration
 
       @git_client_wrapper.add_deployqa_bot_to_repo(repository, permission: :maintainer)
       @git_client_wrapper.add_webhook_to_repo(repository)
+      perform_creation_callback(repository)
 
       ReturnValue.ok(repository)
+    end
+
+    private
+
+    def perform_creation_callback(repository)
+      wrapped_repo = Plugins::Adapters::NewRepo.build(repository.project, repository.name)
+      Plugins::Entry::OnRepoCreation.new(wrapped_repo).call
     end
   end
 end
