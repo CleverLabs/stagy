29082ff4a8f689e3de5681cdce97a20f39f02f69
Eugene Haisenka <evheny0@gmail.com>
2020-06-16 13:31:27 +0300

    Improve instances on dashboard

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -7,5 +7,6 @@ if ENV["DATADOG"]
   Datadog.configure do |config|
     config.tracer hostname: Net::HTTP.get(URI("http://169.254.169.254/latest/meta-data/local-ipv4"))
     config.use :rails
+    config.use :sidekiq, service_name: "deployqa-worker"
   end
 end
