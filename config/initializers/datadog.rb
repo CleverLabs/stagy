1f4503e4a671711b68a0a8dda0e80511ae2565a2
Eugene Haisenka <evheny0@gmail.com>
2020-06-24 11:30:52 +0300

    Fix datadog initializer

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -3,7 +3,7 @@
 require "ddtrace"
 require "net/http"
 
-if ENV["DATADOG"]
+if ENV["DATADOG"].present?
   Datadog.configure do |config|
     config.tracer hostname: Net::HTTP.get(URI("http://169.254.169.254/latest/meta-data/local-ipv4"))
     config.use :rails
