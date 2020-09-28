a44289a91f345fb14f7f346ae2b32ffd14fe6a88
Eugene Haisenka <evheny0@gmail.com>
2020-09-28 15:46:42 +0300

    Add 404. Small layout changes

--- config/initializers/assets_cache_headers.rb
+++ config/initializers/assets_cache_headers.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 # This will affect assets served from /app/assets
-Rails.application.config.static_cache_control = 'public, max-age=31536000'
+Rails.application.config.static_cache_control = "public, max-age=31536000"
 
 # This will affect assets in /public, e.g. webpacker assets.
 Rails.application.config.public_file_server.headers = {
-  'Cache-Control' => 'public, max-age=86400',
-  'Expires' => 1.day.from_now.to_formatted_s(:rfc822)
+  "Cache-Control" => "public, max-age=604800",
+  "Expires" => 7.day.from_now.to_formatted_s(:rfc822)
 }
