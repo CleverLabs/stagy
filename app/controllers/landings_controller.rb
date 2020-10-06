99cbf5b14ea6c445397f900bb08f9e992077a441
Eugene Haisenka <evheny0@gmail.com>
2020-10-06 19:14:35 +0300

    Use uniq landing cache key for every deployment

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class LandingsController < ApplicationController
+  CACHE_KEY = SecureRandom.uuid
+
   skip_before_action :login_if_not
 
   layout "landing"
 
   caches_page :roles, :pricing, :faq
-  caches_action :index, if: -> { !authenticated? }
+  caches_action :index, if: -> { !authenticated? }, cache_path: -> { "cached-views.landings#index.#{CACHE_KEY}" }, expires_in: 7.days
 
   def index
     return redirect_to projects_path if authenticated? && Rails.env.production?
-
-    render :index
   end
 
   def roles
@@ -19,16 +19,11 @@ class LandingsController < ApplicationController
     raise unless %w[for-managers for-qa for-sales for-developers].include? @role_name
 
     @texts = I18n.t("landing.roles.#{@role_name}.how-it-helps")
-    render :roles
   end
 
-  def pricing
-    render :pricing
-  end
+  def pricing; end
 
-  def faq
-    render :faq
-  end
+  def faq; end
 
   def create
     return redirect_to("/") if params[:email].blank?
